4b3b39e6dd711850a977e1c567e28c3a9fa1ef88
danyAmaral <danyamaral27@gmail.com>
2020-01-14 23:06:58 -0300

    Aula 2 - Criação do backend node.js

---
+++ backend/src/routes.js
@@ -0,0 +1,11 @@
+const {Router} = require('express');
+const axios = require('axios');
+const routes = Router();
+const DevController = require('./controllers/DevController')
+const SearchController = require('./controllers/SearchController')
+
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+routes.get('/search', SearchController.index);
+
+module.exports = routes;
