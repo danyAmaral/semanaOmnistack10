1db8dbb5bf42c43017f6889c22cf5e68bd70a02f
danyAmaral <danyamaral27@gmail.com>
2020-01-16 00:43:40 -0300

    Aula 3 - Criação da aplicação em React

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const mongoose = require('mongoose')
-const routes = require('./routes')
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes');
 const app = express();
 mongoose.connect('mongodb+srv://danielle:danielle10@cluster0-qnlga.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
